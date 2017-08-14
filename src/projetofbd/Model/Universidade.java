4d556fe737964480b9377e42ce3d78ef5bc8943f
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2017-08-13 21:58:49 -0300

    Acionando a Tela Universidade

---
+++ src/projetofbd/Model/Universidade.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetofbd.Model;
+
+/**
+ *
+ * @author lucas
+ */
+public class Universidade {
+    
+    private int Cod_Univer;
+    private String Nome_Univer;
+    private String Sigla;
+    private String UF;
+
+    public int getCod_Univer() {
+        return Cod_Univer;
+    }
+
+    public void setCod_Univer(int Cod_Univer) {
+        this.Cod_Univer = Cod_Univer;
+    }
+
+    public String getNome_Univer() {
+        return Nome_Univer;
+    }
+
+    public void setNome_Univer(String Nome_Univer) {
+        this.Nome_Univer = Nome_Univer;
+    }
+
+    public String getSigla() {
+        return Sigla;
+    }
+
+    public void setSigla(String Sigla) {
+        this.Sigla = Sigla;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+    
+    
+    
+}
